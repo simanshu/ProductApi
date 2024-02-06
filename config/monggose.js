bb9a94eea7fc6ada91292713a1c265f87c2f8e52
SIMANSHU CHAUBEY <76093178+simanshu@users.noreply.github.com>
2024-02-06 20:45:12 +0530

    Update monggose.js

--- config/monggose.js
+++ config/monggose.js
@@ -2,10 +2,7 @@ const mongoose=require('mongoose')
 
 //mongoose url
 const url='mongodb://127.0.0.1:27017/ProductApi'
-mongoose.connect(url, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
+mongoose.connect(url)
 const db=mongoose.connection
 
 db.once('open',()=>{
