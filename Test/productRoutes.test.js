4ba9373af76ea73d4c81eaac78f4931bc85334de
SIMANSHU CHAUBEY <76093178+simanshu@users.noreply.github.com>
2024-02-06 18:57:00 +0530

    Update productRoutes.test.js

--- Test/productRoutes.test.js
+++ Test/productRoutes.test.js
@@ -1,21 +1,18 @@
-const chai=import("chai")
-const chaiHttp=require("chai-http")
-const app=require("../index")
+const chai=require('chai')
+let chaiHttp=require('chai-http')
+const app=require('../index')
 const Product=require("../model/Product")
+
 
 chai.use(chaiHttp)
 const expect=chai.expect
 
 
 describe("Product Model Tests", () => {
-    // Clear the database before running tests
-    before(async () => {
-      await Product.deleteMany()
-    })
-
     //Test store data
     it("Sucessfull store product",async()=>{
         const productData={
+            id:10,
             name:"Test",
             description:"Test",
             price:34000,
@@ -28,64 +25,46 @@ describe("Product Model Tests", () => {
                 }
             ]
         }
-        const res=await chai.request(app).post("/api/product").send(productData)
+        const res=await chai.request(app).post("/api/product/createproduct").send(productData)
         expect(res).to.have.status(201)
         expect(res.body).to.have.property('name').equal('Test')
     })
     it("sucessfull update a product", async ()=>{
-        const productData={
-            name:"Test Orignal",
-            description:"Test Orignal",
+        const product=new Product({
+            name:"Test",
+            description:"Test",
             price:34000,
             variants:[
                 {
-                    name:"Test1 Orignal",
+                    name:"Test1",
                     SKU:"test2",
                     additionalcost:78,
                     stockcount:20
                 }
             ]
-        }
-        const createProduct=await chai.request(app).post('/api/product').send(productData)
-        const updateproductData={
-            name:"Test update",
-            description:"Test update",
-            price:34000,
-            variants:[
-                {
-                    name:"Test1 update",
-                    SKU:"test2",
-                    additionalcost:78,
-                    stockcount:20
-                }
-            ]
-        }
-        const res=await chai.request(app).put(`/api/product/${createProduct.body.id}`).send(updateproductData)
+        })
+        await product.save()
+        const res=await chai.request(app).put("/api/product/"+ product._id).send(product)
 
         expect(res).to.have.status(200)
-        expect(res.body).to.have.property('name').equal('Test update')
 
     })
-    it("sucessfull delete a product", async()=>{
-        const productData={
-            name:"Test Delete",
-            description:"Test Delete",
+    it('Delete a product',async()=>{
+        const product=new Product({
+            name:"Test",
+            description:"Test",
             price:34000,
             variants:[
                 {
-                    name:"Test1 Delete",
+                    name:"Test1",
                     SKU:"test2",
                     additionalcost:78,
                     stockcount:20
                 }
             ]
-        }
-        const createProduct=await chai.request(app).post('/api/product').send(productData)
-
-        const res=await chai.request(app).delete(`/api/product/${createProduct.body.id}`)
-
-        expect(res).to.have.status(200)
-        expect(res.body).to.have.property('message').equal('Test Delete')
-
+        })
+        await product.save()
+        const res=await chai.request(app).delete("/api/product/"+ product._id)
+        expect(res.status).to.equal(200)
     })
 })
