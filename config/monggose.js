1dc75f11219c68fe8099c306254d6a53328cfccf
SIMANSHU CHAUBEY <76093178+simanshu@users.noreply.github.com>
2024-02-10 21:24:48 +0530

    Update monggose.js
    update

--- config/monggose.js
+++ config/monggose.js
@@ -2,9 +2,15 @@ const mongoose=require('mongoose')
 
 //mongoose url
 const url='mongodb://127.0.0.1:27017/ProductApi'
-mongoose.connect(url)
-const db=mongoose.connection
 
-db.once('open',()=>{
-    console.log("Database Connected")
-})
+async function connectToDatabase() {
+    try {
+      await mongoose.connect(url);
+      console.log('Connected to MongoDB');
+    } catch (error) {
+      console.error('Error connecting to MongoDB:', error);
+      throw error; // Rethrow the error to halt server startup
+    }
+  }
+  
+  module.exports = connectToDatabase; // Export the connectToDatabase function
